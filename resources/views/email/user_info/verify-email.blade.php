13fca86ab48f181040ab67f68f3a0b713ee946f3
github VinousTy <github ys.taka44@gmail.com>
2023-04-09 15:42:26 +0900

    feat:create verified email template

---
+++ resources/views/email/user_info/verify-email.blade.php
@@ -0,0 +1,57 @@
+@component('mail::layout')
+
+{{-- Header --}}
+@slot('header')
+    @component('mail::header', ['url' => config('app.url')])
+        {{ config('app.name') }}
+    @endcomponent
+@endslot
+
+{{ __('mail.verify_email.greet') }}<br>
+<br>
+{{ __('mail.verify_email.notice_apply') }}<br>
+<br>
+@isset($actionText)
+    <?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+    ?>
+    @component('mail::button', ['url' => $actionUrl, 'color' => $color])
+        {{ $actionText }}
+    @endcomponent
+@endisset
+<br>
+{{ __('mail.verify_email.expire') }}<br>
+{{ __('mail.verify_email.expire_guide') }}<br>
+<br>
+{{ __('mail.verify_email.notice') }}<br>
+<br>
+{{ config('mail.from.name' )}}
+
+{{-- Subcopy --}}
+@isset($actionText)
+    @slot('subcopy')
+        @component('mail::subcopy')
+            @lang(
+                "もし、\":actionText\"ボタンをクリックできない場合には, 下記URLをコピーして、ブラウザに貼り付けてください。",
+                [
+                    'actionText' => $actionText,
+                ]
+            ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+        @endcomponent
+    @endslot
+@endisset
+
+{{-- Footer --}}
+@slot('footer')
+    @component('mail::footer')
+        © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+    @endcomponent
+@endslot
+@endcomponent
