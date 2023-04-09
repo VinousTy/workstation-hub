8c416300a7900d8444ec9bd48707d5d99eefa375
github VinousTy <github ys.taka44@gmail.com>
2023-04-09 14:55:25 +0900

    feat:create register controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', LoginController::class)->name('login');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/register', RegisterController::class)->name('register');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
