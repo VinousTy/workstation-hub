f1c4f6e6de69a90057b48550e0388494db53a8dc
github VinousTy <github ys.taka44@gmail.com>
2023-04-09 16:39:02 +0900

    feat:create password reset functional

---
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Rules\AlphaNumSymbol;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Password as PasswordRule;
+use Symfony\Component\HttpFoundation\Response;
+
+class NewPasswordController extends Controller
+{
+  /**
+     * 新しいパスワード設定
+     * 
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $request->validate([
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', 'string', 'min:8', 'max:255', new AlphaNumSymbol(), PasswordRule::default()],
+        ]);
+
+        $status = Password::reset(
+          $request->only('email', 'password', 'password_confirmation', 'token'),
+          function ($user) use ($request) {
+              $user->forceFill([
+                  'password' => Hash::make($request->password),
+                  'remember_token' => Str::random(60),
+              ])->save();
+          }
+        );
+
+        return $status == Password::PASSWORD_RESET
+                  ? response()->json(['message' => __('passwords.reset')])
+                  : response()->json(['message' => __('passwords.error')], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
@@ -9,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/register', RegisterController::class)->name('register');
 Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->name('verification.verify');
+Route::post('forgot-password', PasswordResetLinkController::class)->name('password.email');
+Route::post('reset-password', NewPasswordController::class)->name('password.reset');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
