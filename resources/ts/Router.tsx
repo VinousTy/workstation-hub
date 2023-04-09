137ba5727a35d84fecf42391ef022d03d63a93fc
github VinousTy <github ys.taka44@gmail.com>
2023-04-09 16:00:37 +0900

    feat:create email authorization send page

--- resources/ts/Router.tsx
+++ resources/ts/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./views/components/header/Header";
 import Login from "./views/pages/auth/Login";
 import Register from "./views/pages/auth/Register";
+import VerifyEmail from "./views/pages/auth/VerifyEmail";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/verify/email" element={<VerifyEmail />} />
       </Routes>
     </BrowserRouter>
   );
