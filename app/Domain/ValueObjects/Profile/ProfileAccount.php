14eb1d1b63c18b49f6106c184219e93f0764fef9
github VinousTy <github ys.taka44@gmail.com>
2023-04-17 20:43:18 +0900

    feat:add valueObjects for profile

---
+++ app/Domain/ValueObjects/Profile/ProfileAccount.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\ValueObjects\Profile;
+
+class ProfileAccount
+{
+    /**
+     * @var string
+     */
+    private string $value;
+
+    /**
+     * @param  string  $value
+     */
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+      return $this->value;
+    }
+}
