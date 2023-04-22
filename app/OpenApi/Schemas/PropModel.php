c0a0f282df206791918c5602ca6a777ea43b0db8
github VinousTy <github ys.taka44@gmail.com>
2023-04-22 19:22:29 +0900

    feat:create a common model when defining a schema

---
+++ app/OpenApi/Schemas/PropModel.php
@@ -0,0 +1,51 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\OpenApi\Schemas;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class PropModel
+{
+  /**
+   * @param Schema $schema
+   * @param string $objectId
+   * @param boolean $nullable
+   * @param boolean $isArray
+   */
+  public function __construct(
+    private readonly Schema $schema,
+    private readonly string $objectId,
+    private readonly bool $nullable,
+    private readonly bool $isArray = false,
+  )
+  {
+  }
+
+  /**
+   * @return Schema
+   */
+  public function getSchema(): Schema
+  {
+    $propertySchema = $this->nullable ? $this->schema->nullable() : $this->schema;
+
+    return $this->isArray ? Schema::array($this->objectId)->items($propertySchema) : $propertySchema;
+  }
+
+  /**
+   * @return string
+   */
+  public function getObjectId(): string
+  {
+    return $this->objectId;
+  }
+
+  /**
+   * @return boolean
+   */
+  public function isNullable(): bool
+  {
+    return $this->nullable;
+  }
+}
