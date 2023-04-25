dcc40d5369323d99f2a1a59e05d95a429bda00e4
github VinousTy <github ys.taka44@gmail.com>
2023-04-25 09:17:07 +0900

    feat:test

---
+++ app/UseCases/Auth/ChangeEmail/ChangeEmailUseCase.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UseCases\Auth\ChangeEmail;
+
+class ChangeEmailUseCase implements ChangeEmailUseCaseInterface
+{
+    public function execute()
+    {
+
+    }
+
+    private function generateToken()
+    {
+      
+    }
+}
