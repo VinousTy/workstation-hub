e52973748d73bdb3eaf4395a9f0ed56efbfa310a
github VinousTy <github ys.taka44@gmail.com>
2023-04-26 08:22:11 +0900

    feat:send email to confirm email address change

---
+++ app/Notifications/VerifyUpdateEmailNotification.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class VerifyUpdateEmailNotification extends Notification
+{
+    use Queueable;
+
+    private string $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+            ->markdown('email.user_info.verify-update-email')
+            ->subject(__('mail.verify_update_email.subject'))
+            ->action(
+                __('mail.verify_update_email.button_text'),
+                route('api.user.email.update', [
+                    'token' => $this->token,
+                ])
+            );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
