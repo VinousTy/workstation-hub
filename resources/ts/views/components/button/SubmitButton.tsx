c7a6965bfe109fa5fda42ea356029ac4b2eb36b4
github VinousTy <github ys.taka44@gmail.com>
2023-06-11 09:42:22 +0900

    feat:change style by user or admin

--- resources/ts/views/components/button/SubmitButton.tsx
+++ resources/ts/views/components/button/SubmitButton.tsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { USER_TYPE } from "../../../utils/enums/user/UserType";
 
 interface PROPS {
   label: string;
+  userType?: USER_TYPE;
   clickEvent?: () => Promise<void>;
 }
 
 const SubmitButton: React.FC<PROPS> = (props) => {
   const buttonType = props.clickEvent ? "button" : "submit";
+  const buttonColor = props.userType === 1 ? "bg-blue-800" : "bg-green-800";
+  const buttonHover =
+    props.userType === 1 ? "hover:bg-blue-700" : "hover:bg-green-700";
 
   return (
     <div>
       <button
         type={buttonType}
         onClick={buttonType === "button" ? props.clickEvent : undefined}
-        className="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full transition"
+        className={`${buttonColor} ${buttonHover} text-white font-bold py-2 px-4 rounded w-full transition`}
       >
         {props.label}
       </button>
